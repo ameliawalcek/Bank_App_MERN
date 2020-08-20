d78552dc7337650a7272f5bddfbdc89b0174d10b
ameliawalcek <ameliawalcek@gmail.com>
2020-08-20 13:05:18 +0300

    before refactor

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -7,8 +7,8 @@ function Chart(props) {
     return (
         <div id='chart'>
             <PieChart
-                data={categoryInfo.map(d => {
-                    return { title: d.name, 'value': props.categorySum(d.name), color: d.color }
+                data={categoryInfo.map(c => {
+                    return { title: c.name, 'value': props.categorySum(c.name), color: c.color }
                 })} lineWidth={20} radius={24} />
         </div>
     )
